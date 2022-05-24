c6e55244b376b27f62aa1129f329ad6cc0db2758
Igor Martins <igorm573@gmail.com>
2022-05-24 16:36:53 -0300

    Create Department entite

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,61 +10,15 @@ import java.text.SimpleDateFormat;
 
 import db.DB;
 import db.DBException;
+import model.entities.Departament;
 
 public class Program {
 
 	
 	public static void main(String[] args) {
 		
-		
-		Connection conn = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/yyyy");
-		
-		try {
-			
-			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"INSERT INTO seller"
-					+"(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-				    +"VALUES(?, ?, ?, ?, ?)"
-					, PreparedStatement.RETURN_GENERATED_KEYS
-					);
-			st.setString(1, "Igor Mart");
-			st.setString(2, "igor@test.com");
-			st.setDate(3, new Date(sdf.parse("03/10/1996").getTime()));
-			st.setDouble(4, 3000.00);
-			st.setInt(5, 4);
-			
-			int rows = st.executeUpdate();
-			
-			if(rows > 0) {
-				
-				rs = st.getGeneratedKeys();
-				
-				while(rs.next()) {
-					System.out.println("done! id= "+ rs.getInt(1));
-				}
-				
-			} else {
-				System.out.println("No rows affectds!!!");
-			}
-			
-			
-		} catch (SQLException e) {
-			throw new DBException(e.getMessage());
-		}
-		catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DB.closeStatement(st);
-			DB.closeResultSet(rs);
-			DB.closeConnection();
-		}
+		Departament departament = new Departament(1,"Books");
+		System.out.println(departament);
 		
 	}
 	
